a60de47c25a1492dc9b8f25c87fa025d07d30ba6
Divyanshguptaj <luckys510039@gmail.com>
2025-08-25 00:17:49 +0530

    added controller, routes for problems and udpated auth middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
+import problemRoutes from './routes/problem.routes.js';
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.get("/", (req, res) => {
 
 // Auth routes
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/problems", problemRoutes);
 
 const PORT = process.env.PORT || 8080;
 
