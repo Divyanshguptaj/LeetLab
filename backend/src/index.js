e0503af451d46c5a8cf9155c7d0dcd65d8323fa9
Divyanshguptaj <luckys510039@gmail.com>
2025-09-29 22:37:13 +0530

    updated backend route to connect render2

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,12 +15,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173, https://leetlab-ten.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 
-app.use(cors());  
+// app.use(cors());  
 
 app.get("/", (req, res) => {
   res.send("Hello, welcome to LeetLab");
