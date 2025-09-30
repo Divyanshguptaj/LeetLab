796548b2bc76d90f02cbfae28d8d20a54edfae9d
Divyanshguptaj <luckys510039@gmail.com>
2025-09-30 10:49:49 +0530

    fixed cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://leetlab-ten.vercel.app"],
+  origin: ["http://localhost:5173", "https://leetlab-ten.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
